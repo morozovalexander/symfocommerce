c4b315a8d6e5e36d43053cbad2214ba6eda32df6
alexander <morozov_a@sibers.com>
2015-08-08 22:26:38 +0600

    generate bundles structure

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Eshop\ShopBundle\ShopBundle(),
+            new Eshop\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
