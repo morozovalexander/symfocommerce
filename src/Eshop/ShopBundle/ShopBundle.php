32af6a251f054f2884b565fe248861397f7f3368
alexander <morozov_a@sibers.com>
2015-08-08 22:33:28 +0600

    generate bundles structure

---
+++ src/Eshop/ShopBundle/ShopBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Eshop\ShopBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ShopBundle extends Bundle
+{
+}
---
+++ src/Eshop/UserBundle/UserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Eshop\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+}
