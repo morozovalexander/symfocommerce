9716bd65cb9f8550878d5618f2410369e0d79351
alexander <morozov_a@sibers.com>
2015-09-06 03:10:16 +0600

    make mane page with thumbnails

--- src/Eshop/ShopBundle/Controller/CatalogController.php
+++ src/Eshop/ShopBundle/Controller/CatalogController.php
@@ -15,26 +15,13 @@ class CatalogController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $paginator = $this->get('knp_paginator');
         $categoryRepository = $em->getRepository('ShopBundle:Category');
         $manufacturerRepository = $em->getRepository('ShopBundle:Manufacturer');
 
         $categories = $categoryRepository->findAll();
         $manufacturers = $manufacturerRepository->findAll();
 
-
-        $dql = "SELECT a FROM ShopBundle:Good a";
-        $query = $em->createQuery($dql);
-
-        $limit = $this->getParameter('goods_pagination_count');
-        $goods = $paginator->paginate(
-            $query,
-            $this->get('request')->query->getInt('page', 1),
-            $limit
-        );
-
         return array(
-            'goods' => $goods,
             'categories' => $categories,
             'manufacturers' => $manufacturers
         );
