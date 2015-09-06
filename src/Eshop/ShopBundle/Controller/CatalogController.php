72a38e6f6710547bcedd8bcee2ab033293f78d26
alexander <morozov_a@sibers.com>
2015-09-07 02:50:44 +0600

    add link

--- src/Eshop/ShopBundle/Controller/CatalogController.php
+++ src/Eshop/ShopBundle/Controller/CatalogController.php
@@ -3,13 +3,17 @@
 namespace Eshop\ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class CatalogController extends Controller
 {
     /**
-     * @Route("/")
+     * Lists all Category entities.
+     *
+     * @Route("/", name="index_main")
+     * @Method("GET")
      * @Template()
      */
     public function indexAction()
