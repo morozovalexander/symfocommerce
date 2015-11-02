6b3fb75ddaa76b63328ace9e668a72f0c3af03a1
Alexander Morozov <morozov.alexander@sibers.com>
2015-11-02 18:57:43 +0600

    make cart controller

--- src/Eshop/ShopBundle/Controller/CartController.php
+++ src/Eshop/ShopBundle/Controller/CartController.php
@@ -13,6 +13,61 @@ use Symfony\Component\HttpFoundation\Request;
 class CartController extends Controller
 {
     /**
+     * Lists all Category entities.
+     *
+     * @Route("/showcart", name="showcart")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showCartAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $productRepository = $em->getRepository('ShopBundle:Product');
+        $productsArray = array();
+        $totalSum = 0;
+
+        $cookies = $request->cookies->all();
+
+        if (isset($cookies['cart'])) {
+            $cart = json_decode($cookies['cart']);
+            if ($cart == '') {
+                return array();
+            }
+        } else {
+            return array();
+        }
+
+
+        foreach ($cart as $productId => $productQuantity) {
+            /**
+             * @var Product $product
+             */
+            $product = $productRepository->find((int)$productId);
+            if (is_object($product)) {
+                $productPosition = array();
+
+                $quantity = abs((int)$productQuantity);
+                $price = $product->getPrice();
+                $sum = $price * $quantity;
+
+                $productPosition['product'] = $product;
+                $productPosition['quantity'] = $quantity;
+                $productPosition['price'] = $price;
+                $productPosition['sum'] = $sum;
+                $totalSum += $sum;
+
+                $productsArray[] = $productPosition;
+            }
+        }
+
+
+        return array(
+            'products' => $productsArray,
+            'totalsum' => $totalSum
+        );
+    }
+
+    /**
      * Count cart from cookies
      *
      * @Method("GET")
