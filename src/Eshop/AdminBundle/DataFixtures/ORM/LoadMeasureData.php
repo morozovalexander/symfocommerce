86d5e7464e3f543dd77c847ae54a535c10e89fa9
Alexander Morozov <morozov.alexander@sibers.com>
2015-11-09 14:11:04 +0600

    add  measure fixtures

---
+++ src/Eshop/AdminBundle/DataFixtures/ORM/LoadMeasureData.php
@@ -0,0 +1,56 @@
+<?php
+namespace Eshop\AdminBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Eshop\ShopBundle\Entity\Measure;
+
+class LoadMeasureData implements FixtureInterface, ContainerAwareInterface, OrderedFixtureInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $measureArray = array(
+            'Grammes',
+            'Peaces',
+            'Caps',
+            'Ml',
+            'Packs'
+        );
+
+        //create measures
+        foreach ($measureArray as $measureName) {
+            $measure = new Measure();
+            $measure->setName($measureName);
+            $manager->persist($measure);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 3; // the order in which fixtures will be loaded
+    }
+}
