dc46befe7e63b460407b071f5bd8ed2e81bac5b7
alexander <morozov_a@sibers.com>
2016-01-01 13:50:01 +0600

    add static page entity

---
+++ src/Eshop/ShopBundle/Entity/StaticPage.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Eshop\ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * StaticPage
+ *
+ * @ORM\Table(name="static_page")
+ * @ORM\Entity(repositoryClass="Eshop\ShopBundle\Repository\StaticPageRepository")
+ */
+class StaticPage
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="orderNum", type="boolean", unique=true)
+     */
+    private $orderNum;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return StaticPage
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     * @return StaticPage
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set orderNum
+     *
+     * @param boolean $orderNum
+     * @return StaticPage
+     */
+    public function setOrderNum($orderNum)
+    {
+        $this->orderNum = $orderNum;
+
+        return $this;
+    }
+
+    /**
+     * Get orderNum
+     *
+     * @return boolean 
+     */
+    public function getOrderNum()
+    {
+        return $this->orderNum;
+    }
+}
