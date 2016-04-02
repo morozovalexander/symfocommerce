3404a26b2b71954dd5783211d673b12621dd6c5f
alexander <morozov_a@sibers.com>
2016-04-02 17:33:38 +0700

    update with latest framework recommendations

--- src/Eshop/AdminBundle/Controller/SlideController.php
+++ src/Eshop/AdminBundle/Controller/SlideController.php
@@ -17,7 +17,6 @@ use Eshop\ShopBundle\Form\Type\SlideType;
  */
 class SlideController extends Controller
 {
-
     /**
      * Lists all Slide entities.
      *
@@ -28,74 +27,37 @@ class SlideController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-
         $entities = $em->getRepository('ShopBundle:Slide')->findBy(array(), array('slideOrder' => 'ASC'));
 
         return array(
             'entities' => $entities,
         );
     }
-    /**
-     * Creates a new Slide entity.
-     *
-     * @Route("/", name="admin_slide_create")
-     * @Method("POST")
-     * @Template("AdminBundle:Slide:new.html.twig")
-     */
-    public function createAction(Request $request)
-    {
-        $entity = new Slide();
-        $form = $this->createCreateForm($entity);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($entity);
-            $em->flush();
-
-            return $this->redirect($this->generateUrl('admin_slide_show', array('id' => $entity->getId())));
-        }
-
-        return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        );
-    }
-
-    /**
-     * Creates a form to create a Slide entity.
-     *
-     * @param Slide $entity The entity
-     *
-     * @return \Symfony\Component\Form\Form The form
-     */
-    private function createCreateForm(Slide $entity)
-    {
-        $form = $this->createForm(SlideType::class, $entity, array(
-            'action' => $this->generateUrl('admin_slide_create'),
-            'method' => 'POST',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Create'));
-
-        return $form;
-    }
 
     /**
      * Displays a form to create a new Slide entity.
      *
      * @Route("/new", name="admin_slide_new")
-     * @Method("GET")
+     * @Method({"GET", "POST"})
      * @Template()
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
-        $entity = new Slide();
-        $form   = $this->createCreateForm($entity);
+        $slide = new Slide();
+        $form = $this->createForm('Eshop\ShopBundle\Form\Type\SlideType', $slide);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($slide);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_slide_show', array('id' => $slide->getId()));
+        }
 
         return array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
+            'entity' => $slide,
+            'form' => $form->createView(),
         );
     }
 
@@ -106,20 +68,12 @@ class SlideController extends Controller
      * @Method("GET")
      * @Template()
      */
-    public function showAction($id)
+    public function showAction(Slide $slide)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('ShopBundle:Slide')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Slide entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
+        $deleteForm = $this->createDeleteForm($slide);
 
         return array(
-            'entity'      => $entity,
+            'entity' => $slide,
             'delete_form' => $deleteForm->createView(),
         );
     }
@@ -128,74 +82,24 @@ class SlideController extends Controller
      * Displays a form to edit an existing Slide entity.
      *
      * @Route("/{id}/edit", name="admin_slide_edit")
-     * @Method("GET")
+     * @Method({"GET", "POST"})
      * @Template()
      */
-    public function editAction($id)
+    public function editAction(Request $request, Slide $slide)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('ShopBundle:Slide')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Slide entity.');
-        }
-
-        $editForm = $this->createEditForm($entity);
-        $deleteForm = $this->createDeleteForm($id);
-
-        return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        );
-    }
-
-    /**
-    * Creates a form to edit a Slide entity.
-    *
-    * @param Slide $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Slide $entity)
-    {
-        $form = $this->createForm(SlideType::class, $entity, array(
-            'action' => $this->generateUrl('admin_slide_update', array('id' => $entity->getId())),
-            'method' => 'PUT',
-        ));
-
-        $form->add('submit', 'submit', array('label' => 'Update'));
-
-        return $form;
-    }
-    /**
-     * Edits an existing Slide entity.
-     *
-     * @Route("/{id}", name="admin_slide_update")
-     * @Method("PUT")
-     * @Template("AdminBundle:Slide:edit.html.twig")
-     */
-    public function updateAction(Request $request, $id)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $entity = $em->getRepository('ShopBundle:Slide')->find($id);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find Slide entity.');
-        }
-
-        $deleteForm = $this->createDeleteForm($id);
-        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($slide);
+        $editForm = $this->createForm('Eshop\ShopBundle\Form\Type\SlideType', $slide);
         $editForm->handleRequest($request);
 
-        if ($editForm->isValid()) {
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
             if ($editForm->get('file')->getData() !== null) { // if any file was updated
                 $file = $editForm->get('file')->getData();
-                $entity->removeUpload(); // remove old file, see this at the bottom
-                $entity->setPath(($file->getClientOriginalName())); // set Image Path because preUpload and upload method will not be called if any doctrine entity will not be changed. It tooks me long time to learn it too.
+                $slide->removeUpload(); // remove old file, see this at the bottom
+                $slide->setPath(($file->getClientOriginalName())); // set Image Path because preUpload and upload method will not be called if any doctrine entity will not be changed. It tooks me long time to learn it too.
             }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($slide);
             $em->flush();
 
             $this->get('session')->getFlashBag()->add(
@@ -203,35 +107,30 @@ class SlideController extends Controller
                 'Your changes were saved!'
             );
 
-            return $this->redirect($this->generateUrl('admin_slide_edit', array('id' => $id)));
+            return $this->redirectToRoute('admin_slide_edit', array('id' => $slide->getId()));
         }
 
         return array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
+            'entity' => $slide,
+            'edit_form' => $editForm->createView(),
             'delete_form' => $deleteForm->createView(),
         );
     }
+
     /**
      * Deletes a Slide entity.
      *
      * @Route("/{id}", name="admin_slide_delete")
      * @Method("DELETE")
      */
-    public function deleteAction(Request $request, $id)
+    public function deleteAction(Request $request, Slide $slide)
     {
-        $form = $this->createDeleteForm($id);
+        $form = $this->createDeleteForm($slide);
         $form->handleRequest($request);
 
-        if ($form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $entity = $em->getRepository('ShopBundle:Slide')->find($id);
-
-            if (!$entity) {
-                throw $this->createNotFoundException('Unable to find Slide entity.');
-            }
-
-            $em->remove($entity);
+            $em->remove($slide);
             $em->flush();
         }
 
@@ -241,16 +140,15 @@ class SlideController extends Controller
     /**
      * Creates a form to delete a Slide entity by id.
      *
-     * @param mixed $id The entity id
+     * @param Slide $slide The Slide entity
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm($id)
+    private function createDeleteForm(Slide $slide)
     {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('admin_slide_delete', array('id' => $id)))
+            ->setAction($this->generateUrl('admin_slide_delete', array('id' => $slide->getId())))
             ->setMethod('DELETE')
-            ->getForm()
-        ;
+            ->getForm();
     }
 }
