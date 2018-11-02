3b799fbfb7ff1de72d4adfceb87697b320a2f2dd
Morozov Alexander <webdevelopacc@gmail.com>
2018-11-02 16:55:38 +0700

    add slugger for use in fixtures

---
+++ src/Eshop/FixturesBundle/Utils/Slugger.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Eshop\FixturesBundle\Utils;
+
+class Slugger
+{
+    public static function slugify(string $string): string
+    {
+        return preg_replace('/\s+/', '-', mb_strtolower(trim(strip_tags($string)), 'UTF-8'));
+    }
+}
