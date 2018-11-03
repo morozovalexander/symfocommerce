eac094cbb87870ffe1db186ae7983978652cdb38
Morozov Alexander <webdevelopacc@gmail.com>
2018-11-03 20:00:16 +0700

    add tests for custom slugger

---
+++ tests/AdminBundle/Utils/SluggerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\AdminBundle\Utils;
+
+use Eshop\FixturesBundle\Utils\Slugger;
+use PHPUnit\Framework\TestCase;
+
+class SluggerTest extends TestCase
+{
+    /**
+     * @dataProvider getSlugs
+     */
+    public function testSlugify($string, $slug)
+    {
+        $this->assertSame($slug, Slugger::slugify($string));
+    }
+
+    public function getSlugs()
+    {
+        yield ['Lorem Ipsum', 'lorem-ipsum'];
+        yield ['  Lorem Ipsum  ', 'lorem-ipsum'];
+        yield [' lOrEm  iPsUm  ', 'lorem-ipsum'];
+        yield ['!Lorem Ipsum!', '!lorem-ipsum!'];
+        yield ['lorem-ipsum', 'lorem-ipsum'];
+        yield ['lorem 日本語 ipsum', 'lorem-日本語-ipsum'];
+        yield ['lorem русский язык ipsum', 'lorem-русский-язык-ipsum'];
+        yield ['lorem العَرَبِيَّة‎‎ ipsum', 'lorem-العَرَبِيَّة‎‎-ipsum'];
+    }
+}
