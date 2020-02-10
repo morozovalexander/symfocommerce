c972bbb01b87ba60f08d4576c936e29e876b1deb
Morozov Alexander <webdevelopacc@gmail.com>
2020-02-10 09:40:24 +0700

    update composer.json

--- config/bundles.php
+++ config/bundles.php
@@ -19,6 +19,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
