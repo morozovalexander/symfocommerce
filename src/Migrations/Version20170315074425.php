8588d9491c27ea5b54058fccb304820634719a42
Morozov Alexander <webdevelopacc@gmail.com>
2020-03-08 00:16:34 +0700

    fix migrations

--- src/Migrations/Version20170315074425.php
+++ src/Migrations/Version20170315074425.php
@@ -2,8 +2,9 @@
 
 namespace Application\Migrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\DBALException;
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -12,8 +13,9 @@ class Version20170315074425 extends AbstractMigration
 {
     /**
      * @param Schema $schema
+     * @throws DBALException
      */
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -24,8 +26,9 @@ class Version20170315074425 extends AbstractMigration
 
     /**
      * @param Schema $schema
+     * @throws DBALException
      */
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
