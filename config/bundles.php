22724544c94e53bca7b62cf5b9d423e5ec48288a
Morozov Alexander <webdevelopacc@gmail.com>
2022-10-30 23:44:01 +0600

    update symfony/debug-bundle recipe

--- config/bundles.php
+++ config/bundles.php
@@ -16,7 +16,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
