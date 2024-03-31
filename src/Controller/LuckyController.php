69384074203ce42928ef5eeb671377494b1180f0
Morozov Alexander <webdevelopacc@gmail.com>
2024-03-31 13:11:10 +0500

    use yml file for routing

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,18 +2,17 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
-    #[Route('/lucky/number')]
-    public function number(): Response
+    public function __invoke(): Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
